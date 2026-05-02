ecf3792523273b10ca6241fadf12440bec5d0bbf
ejDeister <ethandeister@gmail.com>
2026-05-02 05:33:40 -0700

    Uncommented backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,11 +1,8 @@
-# Bootstrap problem: this bucket is created by the state-backend module,
-# but Terraform needs it to exist before it can store state here.
-# First run: comment this block out, apply, then uncomment and run `terraform init`.
-#terraform {
-#  backend "s3" {
-#    bucket         = "ethandeister-tfstate"
-#    key            = "shared-infra/terraform.tfstate"
-#    region         = "us-west-2"
-#    dynamodb_table = "terraform-locks"
-#  }
-#}
+terraform {
+  backend "s3" {
+    bucket         = "ethandeister-tfstate"
+    key            = "shared-infra/terraform.tfstate"
+    region         = "us-west-2"
+    dynamodb_table = "terraform-locks"
+  }
+}
